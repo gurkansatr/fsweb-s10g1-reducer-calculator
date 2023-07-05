98fc45f111a88b441e0388699b0a875f237a3430
gurkansatr <gurkan-satir@hotmail.com>
2023-07-05 18:21:04 +0300

    s10g1cevaplar

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,10 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const ADD_MEMORY = "ADD_MEMORY";
+export const APPLY_RECALL_MEMORY = "APPLY_RECALL_MEMORY";
+export const CLEAR_MEMORY = "CLEAR_MEMORY";
 
 export const addOne = () => {
   return ({ type: ADD_ONE });
@@ -9,4 +13,23 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
   return ({ type: APPLY_NUMBER, payload: number });
+}
+
+export const changeOperation = (operator) => {
+  return ({ type: CHANGE_OPERATION, payload: operator });
+}
+
+export const clearDisplay = () => {
+  return ({ type: CLEAR_DISPLAY });
+}
+
+export const addMemory = () => {
+  return ({ type: ADD_MEMORY });
+}
+
+export const applyRecalMemory = () => {
+  return ({ type: APPLY_RECALL_MEMORY });
+}
+export const clearMemory = () => {
+  return ({ type: CLEAR_MEMORY });
 }
